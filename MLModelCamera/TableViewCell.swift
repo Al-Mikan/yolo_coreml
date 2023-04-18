1df1dc467b5c5464dc29dce8a636016030ba9f40
Shingo TOYODA <toyodasinngo@gmail.com>
2023-04-19 01:31:18 +0900

    アプリの遷移を作成

---
+++ MLModelCamera/TableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  TableViewCell.swift
+//  MLModelCamera
+//
+//  Created by Shin Toyo on 2023/04/19.
+//  Copyright © 2023 Shuichi Tsutsumi. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+
+}
+    
